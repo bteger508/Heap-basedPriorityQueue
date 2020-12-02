a2efabb15768b625eb66964fe61be0068a946f04
Ben Eger <bteger@bsu.edu>
2020-12-02 18:02:59 -0500

    Add function stubs for the MaxPriorityQueue class

---
+++ MaxPriorityQueue.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+A MaxPriorityQueue module based on CLRS3, chapter 6.5.
+Developed by Alex Saunters and Ben Eger.
+"""
+
+class MaxPriorityQueue(set):
+    def __init__(self):
+        pass
+
+    def build_max_heap(self, list):
+        pass
+
+    def max_heapify(self):
+        pass
+
+    def insert(self, x):
+        # insert a new elt into the set
+        pass
+
+    def maximum(self):
+        # return the element with the largest key
+        pass
+
+    def extract_max(self):
+        # remove and return the element with the largest key
+        pass
+
+    def increase_key(self, x, k):
+        # increase the value of element x's key to the new value k
+        pass

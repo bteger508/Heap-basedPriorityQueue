1280505b33e1f4231c911ebd766b003a585a4a0e
Ben Eger <bteger@bsu.edu>
2020-12-12 16:26:46 -0500

    Increase documentation

--- MaxPriorityQueue.py
+++ MaxPriorityQueue.py
@@ -89,10 +89,6 @@ class MaxPriorityQueueTest(unittest.TestCase):
     heap2 = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
     heap3 = HeapSort.HeapCapable([-5, -10, -14, -15, -20])
 
-    myHeap = HeapSort.HeapCapable([9001, 27, 16, 25, 14, 15, 20])
-    HeapSort.build_max_heap(myHeap)
-    print(myHeap)
-
     def test_maximum_1(self):
         self.assertEqual(5, maximum(self.heap1))
 
@@ -102,56 +98,86 @@ class MaxPriorityQueueTest(unittest.TestCase):
     def test_maximum_3(self):
         self.assertEqual(-5, maximum(self.heap3))
 
-    def test_extract_max_1(self):
+    def test_extract_max_small_heap(self):
+        """
+        extract_max: general case with a 3 element array
+        """
         self.assertEqual(5, extract_max(HeapSort.HeapCapable([5, 4, 3])))
 
-    def test_extract_max_2(self):
+    def test_extract_max_remove_top_element(self):
+        """
+        extract_max() should remove the top element.
+        """
         actual_heap = HeapSort.HeapCapable([5, 4, 3])
         extract_max(actual_heap)
         expected_heap = HeapSort.HeapCapable([4, 3])
         self.assertEqual(expected_heap, actual_heap)
 
-    def test_extract_max_3(self):
+    def test_extract_max_modify_heap_size(self):
+        """
+        extract_max() should modify the heap_size attribute, not just the array length.
+        """
         actual_heap = HeapSort.HeapCapable([5, 4, 3])
         extract_max(actual_heap)
         self.assertEqual(2, actual_heap.heap_size)
 
-    def test_extract_max_4(self):
+    def test_extract_max_general_case(self):
+        """
+        extract max: general case.
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         extract_max(actual_heap)
         expected_heap = HeapSort.HeapCapable([25, 20, 14, 15, 16])
         self.assertEqual(expected_heap, actual_heap)
 
-    def test_increase_key_1(self):
+    def test_increase_key_middle_element(self):
+        """
+        increase_key(): general case, increasing the key of an element in the middle of the heap
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         increase_key(actual_heap, 3, 35)
         expected_heap = HeapSort.HeapCapable([35, 27, 25, 16, 15, 20])
         self.assertEqual(expected_heap, actual_heap)
 
-    def test_increase_key_2(self):
+    def test_increase_key_last_element(self):
+        """
+        increase_key(): general case, increasing the key of the last element in the heap
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         increase_key(actual_heap, 0, 9001)
         expected_heap = HeapSort.HeapCapable([9001, 16, 25, 14, 15, 20])
         self.assertEqual(expected_heap, actual_heap)
 
-    def test_increase_key_3(self):
+    def test_increase_key_invalid_key(self):
+        """
+        increase_key() should give an error message if the new key is smaller than the existing key.
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         error = increase_key(actual_heap, 0, 24)
         self.assertEqual("new key is smaller than existing key", error)
 
-    def test_insert_1(self):
+    def test_insert_large_key(self):
+        """
+        insert: general case with a large key.
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         insert(actual_heap, 9001)
         expected_heap = HeapSort.HeapCapable([9001, 16, 27, 14, 15, 20, 25])
         self.assertEqual(expected_heap, actual_heap)
 
-    def test_insert_2(self):
+    def test_insert_small_key(self):
+        """
+        insert: general case with a small key.
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         insert(actual_heap, 1)
         expected_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20, 1])
         self.assertEqual(expected_heap, actual_heap)
 
-    def test_insert_3(self):
+    def test_insert_middle_key(self):
+        """
+        insert() should maintain the max-heap property.
+        """
         actual_heap = HeapSort.HeapCapable([27, 16, 25, 14, 15, 20])
         insert(actual_heap, 26)
         expected_heap = HeapSort.HeapCapable([27, 16, 26, 14, 15, 20, 25])
